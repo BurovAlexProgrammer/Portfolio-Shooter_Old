03e2ba71559c5ed227d646cab7f7b29f9eb49d99
Aleksey <burovalexprogrammer@gmail.com>
2022-12-31 07:27:44 +0200

    Implemented Health component

---
+++ Assets/_Project/Scripts/Main/Game/HealthBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Main.Game
+{
+    public abstract class HealthBase : MonoWrapper
+    {
+        [SerializeField] private float _maxValue;
+        [SerializeField] private float _currentValue;
+        
+        public Action LifeEnd;
+
+        public float MaxValue => _maxValue;
+        public float CurrentValue => _currentValue;
+
+        public void SetValue(float value)
+        {
+            _currentValue = value;
+        }
+        
+        public void GetDamage(float value)
+        {
+            if (_currentValue == 0f) return;
+            
+            _currentValue -= value;
+
+            if (_currentValue <= 0f)
+            {
+                _currentValue = 0f;
+                LifeEnd?.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Main/Game/SimpleHealth.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Main.Game.Weapon;
+using UnityEngine;
+
+namespace _Project.Scripts.Main.Game
+{
+    public class SimpleHealth : HealthBase
+    {
+        [SerializeField] private Destruction _destructionPrefab;
+        
+        private void Start()
+        {
+            if (CurrentValue == 0f)
+            {
+                SetValue(MaxValue);
+            }
+
+            LifeEnd += Destruct;
+        }
+
+        private void Destruct()
+        {
+            if (_destructionPrefab == null) return;
+
+            Instantiate(_destructionPrefab, transform.parent);
+            Destroy(gameObject);
+        }
+    }
+}
