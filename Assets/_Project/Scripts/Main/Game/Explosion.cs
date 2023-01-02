7655f2cca17494002715d98af2e2abe008ad3d3e
Aleksey <burovalexprogrammer@gmail.com>
2023-01-02 09:05:54 +0200

    Improved Explosion

--- Assets/_Project/Scripts/Main/Game/Explosion.cs
+++ Assets/_Project/Scripts/Main/Game/Explosion.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Extension;
+﻿using System;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using static _Project.Scripts.Main.Services.Services;
@@ -7,24 +8,65 @@ namespace _Project.Scripts.Main.Game
 {
     public class Explosion : MonoBehaviour
     {
+        [SerializeField] private Dependencies _dependencies = Dependencies.Siblings;
         [SerializeField] private float _radius = 2;
         [SerializeField] private float _force = 0.2f;
         [SerializeField] private float _liftForce = 0.1f;
         [SerializeField] private ForceMode _forceMode = ForceMode.Force;
 
-        private async void Start()
+        private Collider[] _colliders;
+        private List<Rigidbody> _rigidbodies;
+
+        private void Awake()
         {
+            _rigidbodies = new List<Rigidbody>();
             DebugService.CreateExplosionGizmo(transform, _radius);
 
-            Collider[] colliders = Physics.OverlapSphere(transform.position, _radius);
-            Debug.Log("Collider count: "+colliders.Length);
-            foreach (var targetCollider in colliders)
+            switch (_dependencies)
+            {
+                case Dependencies.Siblings:
+                    _colliders = transform.parent == null
+                        ? transform.root.GetComponentsInChildren<Collider>()
+                        : transform.parent.GetComponentsInChildren<Collider>();
+                    break;
+                case Dependencies.Children:
+                    _colliders = transform.GetComponentsInChildren<Collider>();
+                    break;
+                case Dependencies.Colliders:
+                    _colliders = Physics.OverlapSphere(transform.position, _radius);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            foreach (var targetCollider in _colliders)
             {
                 var targetRigidbody = targetCollider.GetComponent<Rigidbody>();
 
-                if (targetRigidbody != null)
-                    targetRigidbody.AddExplosionForce(_force, transform.position, _radius, _liftForce, ForceMode.Impulse);
+                if (targetRigidbody == null) continue;
+                
+                targetRigidbody.WakeUp();
+                _rigidbodies.Add(targetRigidbody);
             }
+        }
+
+        private async void Start()
+        {
+            Debug.Log("Collider count: " + _colliders.Length);
+
+            foreach (var targetRigidbody in _rigidbodies)
+            {
+                await UniTask.NextFrame();
+                targetRigidbody.AddExplosionForce(_force, transform.position, _radius, _liftForce,
+                    _forceMode);
+            }
+        }
+
+        private enum Dependencies
+        {
+            Siblings,
+            Children,
+            Colliders,
         }
     }
 }
