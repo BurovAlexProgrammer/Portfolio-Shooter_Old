b60347b003453a96b7b47135728ba19cdbcd7a68
Aleksey <burovalexprogrammer@gmail.com>
2023-01-13 16:18:49 +0200

    Update PlayerBase.cs

--- Assets/_Project/Scripts/Main/Game/PlayerBase.cs
+++ Assets/_Project/Scripts/Main/Game/PlayerBase.cs
@@ -10,12 +10,13 @@ namespace _Project.Scripts.Main.Game
     [RequireComponent(typeof(HealthBase))]
     [RequireComponent(typeof(CharacterController))]
     [RequireComponent(typeof(AudioSource))]
-    public abstract class PlayerBase : MonoBehaviour
+    public abstract class PlayerBase : MonoBeh
     {
         [SerializeField] private CameraHolder _cameraHolder;
         [SerializeField] private PlayerConfig _config;
         [SerializeField] private GunBase _gun;
         [SerializeField] private bool _canMove;
+        [SerializeField] private bool _canRotate;
         [SerializeField] private bool _canShoot;
         [SerializeField] private SimpleAudioEvent _startPhrase;
         
@@ -41,12 +42,11 @@ namespace _Project.Scripts.Main.Game
             _health = GetComponent<HealthBase>();
             _characterController = GetComponent<CharacterController>();
             _audioSource = GetComponent<AudioSource>();
+            _playerControl = _controlService.Controls.Player;
         }
 
         private void Start()
         {
-            _playerControl = _controlService.Controls.Player;
-
             if (_startPhrase != null)
             {
                 _startPhrase.Play(_audioSource);
@@ -62,12 +62,12 @@ namespace _Project.Scripts.Main.Game
             _rotateInputValue = _playerControl.Rotate.ReadValue<Vector2>();
             _rotateLerpValue = Vector2.Lerp(_rotateLerpValue, _rotateInputValue, _config.RotateLerpTime);
             
-            if (_rotateLerpValue != Vector2.zero)
+            if (_canRotate)
             {
                 Rotate(_rotateLerpValue);
             }
 
-            if (_playerControl.Shoot.IsPressed())
+            if (_canShoot && _playerControl.Shoot.IsPressed())
             {
                 TryShoot();
             }
@@ -78,7 +78,7 @@ namespace _Project.Scripts.Main.Game
             _moveInputValue = _playerControl.Move.ReadValue<Vector2>().normalized;
             _moveLerpValue = Vector2.Lerp(_moveLerpValue, _moveInputValue, _config.MoveLerpTime);
             
-            if (_moveLerpValue != Vector2.zero)
+            if (_canMove)
             { 
                 Move(_moveLerpValue);
             }
@@ -87,43 +87,41 @@ namespace _Project.Scripts.Main.Game
         public void Disable()
         {
             _canMove = false;
+            _canRotate = false;
             _canShoot = false;
         }
 
         public void Enable()
         {
             _canMove = true;
+            _canRotate = true;
             _canShoot = true;
         }
         
         protected virtual void Move(Vector2 inputValue)
         {
             if (!_canMove) return;
+            if (inputValue == Vector2.zero) return;
+            
             var moveVector = inputValue * Time.deltaTime * _config.MoveSpeed;
-            _characterController.Move(transform.right * moveVector.x + transform.forward * moveVector.y);
+            _characterController.Move(_transform.right * moveVector.x + _transform.forward * moveVector.y);
         } 
 
-        protected virtual void Rotate(Vector2 rotation)
+        protected virtual void Rotate(Vector2 inputValue)
         {
             if (!_canMove) return;
+            if (inputValue == Vector2.zero) return;
 
             var delta = Time.deltaTime * _config.RotateSpeed * _settingsService.GameSettings.Sensitivity;
-            _rotationY -= rotation.y * delta;
+            _rotationY -= inputValue.y * delta;
             _rotationY = Math.Clamp(_rotationY, -_config.MaxVerticalAngle, _config.MaxVerticalAngle);
             _cameraHolder.transform.localRotation = Quaternion.Euler(_rotationY, 0f, 0f);
-            transform.Rotate(rotation.x * Vector3.up * delta);
+            transform.Rotate(inputValue.x * Vector3.up * delta);
         }
 
         protected virtual void TryShoot()
         {
-            if (!_canShoot) return;
-
             _gun.TryShoot();
-        }
-
-        protected virtual void Shoot()
-        {
-            
         }
     }
 }
