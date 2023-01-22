acbc8f112ddcd4e787a9b48ab2a7fa9316bb5870
Aleksey <burovalexprogrammer@gmail.com>
2023-01-22 08:02:19 +0200

    Refactor

--- Assets/_Project/Scripts/Main/Services/StatisticService.cs
+++ Assets/_Project/Scripts/Main/Services/StatisticService.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using _Project.Scripts.Extension;
-using _Project.Scripts.Main.Wrappers;
 using Newtonsoft.Json;
 using UnityEngine;
 
@@ -11,64 +9,39 @@ namespace _Project.Scripts.Main.Services
 {
     public class StatisticService : BaseService
     {
-        private Dictionary<StatisticData.RecordName, StatisticData.RecordItem> _statisticRecords;
+        private StatisticData _statisticData;
         private string[] _fieldNames;
         private string _storedFolder;
         private string _storedFolderPath;
 
         public void Init()
         {
+            _statisticData = new StatisticData();
             _storedFolder ??= Application.dataPath + "/StoredData/";
             _storedFolderPath = _storedFolder + "Statistic.data";
             _fieldNames = Enum.GetNames(typeof(StatisticData.RecordName));
-            // _statisticRecords = Enum.GetValues(typeof(StatisticData.RecordName))
-            //     .Cast<StatisticData.RecordName>()
-            //     .ToDictionary(key => key, value => new StatisticData.RecordItem(value.ToString(), value.));
             LoadFromFile();
         }
 
         public void LoadFromFile()
         {
             Directory.CreateDirectory(_storedFolder);
-            
-            try
+
+            if (!File.Exists(_storedFolderPath))
             {
-                if (!File.Exists(_storedFolderPath))
-                {
-                    Debug.LogWarning($"Stored file '{_storedFolderPath}' not found. Created empty statistic file.");
-                    var emptyRecords = JsonConvert.SerializeObject(StatisticData.Records);
-                    File.WriteAllText(_storedFolderPath, emptyRecords);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-            
-            
-            var json = File.ReadAllText(_storedFolderPath);
-            var storedData = JsonConvert.DeserializeObject<Dictionary<StatisticData.RecordName,StatisticData.RecordItem>>(json);
-            // else
-            {
-                
-                // 
-                // 
-                // if (storedData == null)
-                // {
-                //     Debug.LogWarning($"Stored file '{_storedFilePath}' is corrupted. Default settings using instead.");
-                // }
-                // _saved.CopyDataFrom(storedData ?? _default);
+                Debug.LogWarning($"Stored file '{_storedFolderPath}' not found. Created empty statistic file.");
+                var emptyRecords = JsonConvert.SerializeObject(_statisticData);
+                File.WriteAllText(_storedFolderPath, emptyRecords);
             }
 
-            // _current ??= ScriptableObject.CreateInstance<T>();
-            // _current.CopyDataFrom(_saved);
+            var json = File.ReadAllText(_storedFolderPath);
+            _statisticData = JsonConvert.DeserializeObject<StatisticData>(json);
         }
 
-        // public void SaveToFile()
-        // {
-        //     var data = Serializer.ToJson(_current);
-        //     File.WriteAllText(_storedFilePath, data);
-        // }
+        public void SaveToFile()
+        {
+            var data = JsonConvert.SerializeObject(_statisticData);
+            File.WriteAllText(_storedFolderPath, data);
+        }
     }
 }
--- Assets/_Project/Scripts/Main/StatisticData.cs
+++ Assets/_Project/Scripts/Main/StatisticData.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using static _Project.Scripts.Main.StatisticData.RecordName;
 
 namespace _Project.Scripts.Main
 {
-    public static class StatisticData
+    [Serializable]
+    public class StatisticData
     {
-        public static readonly Dictionary<RecordName, RecordItem> Records;
+        public Dictionary<RecordName, string> Records;
 
+        private static string DefaultRecordValue(RecordName recordName) =>
+            RecordTypes[recordName] switch
+            {
+                DataType.Bool => "false",
+                DataType.Float => "0",
+                DataType.Integer => "0",
+                DataType.String => "",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+        public StatisticData()
+        {
+            Records = 
+                Enum.GetValues(typeof(RecordName))
+                .Cast<RecordName>()
+                .ToDictionary(key => key, DefaultRecordValue);
+        } 
+        
+        
+        [JsonConverter(typeof(StringEnumConverter))]
         public enum RecordName
         {
             GameSessionCount,
@@ -27,79 +49,15 @@ namespace _Project.Scripts.Main
             Float,
             String,
         }
-
-        static StatisticData()
+        
+        private static readonly Dictionary<RecordName, DataType> RecordTypes = new ()
         {
-            Records = new Dictionary<RecordName, RecordItem>();
-            AddRecord(GameSessionCount, DataType.Integer);
-            AddRecord(LongestGameSession, DataType.Float);
-            AddRecord(AverageLongGameSession, DataType.Float);
-            AddRecord(FireCount, DataType.Integer);
-            AddRecord(Movement, DataType.Float);
-            AddRecord(KillMonsterCount, DataType.Integer);
-        }
-
-        private static void AddRecord(RecordName recordName, DataType dataType)
-        {
-            Records.Add(recordName, new RecordItem(recordName, dataType));
-        }
-
-        public class RecordItem
-        {
-            [JsonConverter(typeof(StringEnumConverter))]
-            public RecordName Name;
-            public string Value;
-            [JsonConverter(typeof(StringEnumConverter))]
-            public DataType DataType;
-
-            private float _floatValue;
-            private float _intValue;
-            private bool _boolValue;
-
-
-            public RecordItem(RecordName recordName, DataType dataType)
-            {
-                DataType = dataType;
-
-                switch (DataType)
-                {
-                    case DataType.Bool:
-                        _boolValue = false;
-                        Value = _boolValue.ToString();
-                        break;
-                    case DataType.Integer:
-                        _intValue = 0;
-                        Value = _intValue.ToString();
-                        break;
-                    case DataType.Float:
-                        _floatValue = 0f;
-                        Value = _floatValue.ToString();
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(dataType), dataType, null);
-                }
-            }
-
-            public RecordItem(RecordName recordName, DataType dataType, string value) : this(recordName, dataType)
-            {
-                switch (dataType)
-                {
-                    case DataType.Bool:
-                        _boolValue = bool.Parse(value);
-                        Value = _boolValue.ToString(); 
-                        break;
-                    case DataType.Integer:
-                        _intValue = float.Parse(value);
-                        Value = _intValue.ToString();
-                        break;
-                    case DataType.Float:
-                        _floatValue = float.Parse(value);
-                        Value = _floatValue.ToString();
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(dataType), dataType, null);
-                }
-            }
-        }
+            {GameSessionCount, DataType.Integer},
+            {LongestGameSession, DataType.Float},
+            {AverageLongGameSession, DataType.Float},
+            {FireCount, DataType.Integer},
+            {Movement, DataType.Float},
+            {KillMonsterCount, DataType.Integer},
+        };
     }
 }
